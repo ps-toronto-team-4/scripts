2f9b7f42c7a88c658c4bd0e744a92c780f41b4e9
jere-mie <jere-mie@users.noreply.github.com>
2022-07-20 12:38:07 -0400

    budgets now work for enum change

--- generators/generate-budgets.py
+++ generators/generate-budgets.py
@@ -1,8 +1,8 @@
 out = "INSERT INTO budgets(month, year, user_id)\nVALUES\n"
 
 for year in [2018, 2019, 2020, 2021, 2022]:
-    for month in range(1, 13):
-        if year == 2022 and month > 6:
+    for month in range(0, 12):
+        if year == 2022 and month > 5:
             break
         out += f"({month}, {year}, 1),\n"
 out = out[:-2]+'\n;\nCOMMIT;'
