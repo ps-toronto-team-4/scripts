59bb0d4e1c00c6206f28b5b9f855678e5eaa3809
Jeremie <jeremiedevelops@gmail.com>
2022-08-08 17:12:48 -0400

    budget categories goes up to august now

--- generators/generate-data-for-user.py
+++ generators/generate-data-for-user.py
@@ -76,7 +76,7 @@ VALUES"""
     def get_random_amount():
         return round(uniform(100.00, 1000.00), 2)
 
-    for budget in range(1+((userId-1)*8), 8+((userId-1)*8)):
+    for budget in range(1+((userId-1)*8), 9+((userId-1)*8)):
         for category in range(1+((userId-1)*8), 9+((userId-1)*8)):
             if category_occurs():
                 amount = get_random_amount()
